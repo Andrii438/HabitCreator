41d7534f76069fc801e79c7216a7935205d9acc8
Andrii <380679724615@mail.ru>
2023-12-14 12:36:42 +0100

    first commit

---
+++ src/main/java/com/tracker/habit/habitCreator/HabitCreatorApplication.java
@@ -0,0 +1,13 @@
+package com.tracker.habit.habitCreator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabitCreatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HabitCreatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tracker/habit/habitCreator/HabitCreatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tracker.habit.habitCreator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabitCreatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
